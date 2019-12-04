1ee3ab8b88ccd61f8ec0ad5a337972dba0c3084a
Arian Moser <arian.moser@hpe.com>
2019-12-04 23:26:03 +0100

    Finishes challenge 2 of day four

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # AdventOfCode
+My solutions for the challenges of the
+[AdventOfCode](https://adventofcode.com/) calendar.
--- src/04/challenge02.py
+++ src/04/challenge02.py
@@ -1,6 +1,6 @@
 
 def main():
-    print("AdventOfCode - 04 - Challenge 01")
+    print("AdventOfCode - 04 - Challenge 02")
     start_value = 165432
     end_value = 707912
     count = 0
@@ -15,15 +15,19 @@ def check_duplicate(number):
     index = 0
     str_number = str(number)
     while index < len(str_number)-1:
-        if str_number[index] == str_number[index+1]:
-            return True
+        try:
+            if str_number[index-1] != str_number[index] and str_number[index] == str_number[index+1] and str_number[index + 2] != str_number[index]:
+                return True
+        except Exception:
+            if str_number[index] == str_number[index+1]:
+                return True
         index += 1
     return False
 
 def check_decreasing(number):
     index = 0
     str_number = str(number)
-    while index < len(str_number)-1:
+    while index < len(str_number)-1 :
         if str_number[index] > str_number[index+1]:
             return True
         index += 1
@@ -31,4 +35,12 @@ def check_decreasing(number):
 
 
 if __name__ == "__main__":
+    if check_duplicate(124456):
+        print("Duplicate")
+    else:
+        print("No Duplicate")
+    if check_decreasing(124456):
+        print("Decreasing")
+    else:
+        print("Not Decreasing ")
     main()
