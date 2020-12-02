52a250516cdbb8a5de9aff27d9c139c9301f39dc
ArianMoser <arian_moser@yahoo.de>
2020-12-02 16:09:32 +0100

    Updated day 2 - improves the code of day2

--- src/2020/02/day2.py
+++ src/2020/02/day2.py
@@ -1,35 +1,35 @@
 import re
 import sys
 
-def task1(file_name):
+def task1(c_min, c_max, letter_occurence):
+    if c_min <= len(letter_occurence) <= c_max:
+        return 1
+    return 0
+
+
+def task2(c_min, c_max, letter_positions):
+    if bool(c_min-1 in letter_positions) != bool(c_max-1 in letter_positions):
+        return 1
+    return 0
+
+
+def parse_input(file_name, task2_bool):
+    valid_passwords = 0
     with open(file_name) as f:
-        valid_passwords = 0
         for line in f:
             splitted_line= line.split(" ")
             c_min = int(splitted_line[0].split("-")[0])
             c_max = int(splitted_line[0].split("-")[1])
             letter = splitted_line[1][0]
             txt = splitted_line[2]
-            letter_occurence = re.findall(re.compile(letter), txt)
-            if c_min <= len(letter_occurence) <= c_max:
-                valid_passwords = valid_passwords+1
-
-        print("Task1: Found {} valid passwords.".format(valid_passwords))
-
-def task2(file_name):
-    with open(file_name) as f:
-        valid_passwords = 0
-        for line in f:
-            splitted_line= line.split(" ")
-            c_min = int(splitted_line[0].split("-")[0])
-            c_max = int(splitted_line[0].split("-")[1])
-            letter = splitted_line[1][0]
-            txt = splitted_line[2]
-            letter_positions = [x.span()[0] for x in re.finditer(re.compile(letter), txt)]
-            if bool(c_min-1 in letter_positions) != bool(c_max-1 in letter_positions):
-                valid_passwords = valid_passwords+1
-
-        print("Task2: Found {} valid passwords.".format(valid_passwords))
+            if task2_bool:
+                letter_positions = [x.span()[0] for x in re.finditer(re.compile(letter), txt)]
+                valid_passwords = valid_passwords + task2(c_min, c_max, letter_positions)
+            else:
+                letter_occurence = re.findall(re.compile(letter), txt)
+                valid_passwords = valid_passwords + task1(c_min, c_max, letter_occurence)
+    print("Found {} valid passwords.".format(valid_passwords))
+                    
 
 def main():
     # default values
@@ -44,10 +44,7 @@ def main():
         print("(Parameter Usage: dayx.py <<file_name>> <<task2 (True|False)>>)")
     print("Using values: \n  Filename: {} \n  Task2: {}".format(file_name, task2_bool))
     print("**************************\n")
-    if task2_bool:
-        task2(file_name)
-    else:
-        task1(file_name)
+    parse_input(file_name, task2_bool)
 
 
 if __name__ == "__main__":
