a978716e6d7e663d6aa23977a9426ecfc9143d97
Ari <ari@linux.fritz.box>
2020-12-06 21:08:58 +0100

    2020: Answers day 6 partly - Task 1 is completed - Task 2 is solved partly

---
+++ src/2020/06/day06.py
@@ -0,0 +1,52 @@
+import sys
+
+
+def main():
+    file_name = "input.txt"
+    task2_bool = False
+    print("Challenge day 4: \n")
+    print("**************************")
+    try:
+        file_name = sys.argv[1]
+        task2_bool = True if sys.argv[2].lower() in ["true", "on", "1", "yes"] else False
+    except Exception as e:
+        print("(Parameter Usage: dayx.py <<file_name>> <<task2 (True|False)>>)")
+    print("Using values: \n  Filename: {} \n  Task2: {}".format(file_name, task2_bool))
+    print("**************************\n")
+    if task2_bool:
+        print("Answers given: {}".format(task2(file_name)))
+    else:
+        print("Answers given: {}".format(task1(file_name)))
+
+
+def task1(file_name):
+    answers = 0
+    with open(file_name) as f:
+        f_content = f.read()
+        groups = f_content.split("\n\n")
+        for group in groups:
+            group = set(group.replace('\n', ''))
+            ans = set(group)
+            answers += len(ans)
+    return answers
+
+
+def task2(file_name):
+    answers = 0
+    with open(file_name) as f:
+        f_content = f.read()
+        groups = f_content.split("\n\n")
+        for group in groups:
+            person = group.split("\n")
+            ans = set()
+            for p in person: 
+                if len(ans) == 0:
+                    ans = set(p)
+                else:
+                    ans.intersection_update(set(p))
+            answers += len(ans)
+    return answers
+
+
+if __name__ == "__main__":
+    main()
