fc126732f03ac897a16b3a79742243ae41066dd6
ArianMoser <moser.arian@gmail.com>
2024-07-27 15:25:22 +0200

    Solves day02 part 2 of 2016

--- src/2016/day02.py
+++ src/2016/day02.py
@@ -3,15 +3,31 @@ import sys
 
 INPUT_DIR = os.path.join(os.path.dirname(__file__), 'inputs')
 NUMBER_PAD = {
-        (-1,  1): 1,
-        ( 0,  1): 2,
-        ( 1,  1): 3,
-        (-1,  0): 4,
-        ( 0,  0): 5,
-        ( 1,  0): 6,
-        (-1, -1): 7,
-        ( 0, -1): 8,
-        ( 1, -1): 9,
+        (-1,  1): '1',
+        (+0,  1): '2',
+        (+1,  1): '3',
+        (-1,  0): '4',
+        (+0,  0): '5',
+        (+1,  0): '6',
+        (-1, -1): '7',
+        (+0, -1): '8',
+        (+1, -1): '9',
+}
+
+NUMBER_PAD_EXT = {
+        (+0,  2): '1',
+        (-1,  1): '2',
+        (+0,  1): '3',
+        (+1,  1): '4',
+        (-2,  0): '5',
+        (-1,  0): '6',
+        (+0,  0): '7',
+        (+1,  0): '8',
+        (+2,  0): '9',
+        (-1, -1): 'A',
+        (+0, -1): 'B',
+        (+1, -1): 'C',
+        (+0, -2): 'D',
 }
 
 
@@ -23,7 +39,7 @@ def prepare_input_file(filename=os.path.basename(__file__).replace('.py',
     return lines
 
 
-def calculate_coords(coords, instructions):
+def calculate_coords(coords, instructions, part2=False):
     for instr in instructions:
         new_coords = coords.copy()
         if instr == 'U':
@@ -34,8 +50,12 @@ def calculate_coords(coords, instructions):
             new_coords[0] += 1
         elif instr == 'L':
             new_coords[0] -= 1
-        if abs(new_coords[0]) < 2 and abs(new_coords[1]) < 2:
-            coords = new_coords
+        if not part2:
+            if tuple(new_coords) in NUMBER_PAD.keys():
+                coords = new_coords
+        else:
+            if tuple(new_coords) in NUMBER_PAD_EXT.keys():
+                coords = new_coords
     return coords
 
 
@@ -51,8 +71,16 @@ def main():
     part1 = ''
     for line in lines:
         coords = calculate_coords(coords, line)
-        part1 += str(NUMBER_PAD[tuple(coords)])
+        part1 += NUMBER_PAD[tuple(coords)]
     print(f"Part 1: {part1}")
+
+    # part 2
+    coords = [-2, 0]
+    part2 = ''
+    for line in lines:
+        coords = calculate_coords(coords, line, True)
+        part2 += NUMBER_PAD_EXT[tuple(coords)]
+    print(f"Part 2: {part2}")
 
 
 if __name__ == '__main__':
