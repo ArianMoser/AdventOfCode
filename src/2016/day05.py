ed9046929d51251fd431db488599e1194cd8a901
ArianMoser <moser.arian@gmail.com>
2024-07-27 18:39:52 +0200

    Solves part 2 of day05 (AoC 2016)

--- src/2016/day05.py
+++ src/2016/day05.py
@@ -13,14 +13,30 @@ def main():
     i = 0
     for _ in range(8):
         while True:
-            hashed = hashlib.md5(f"{inp}{str(i)}".encode())
-            if hashed.hexdigest().startswith("00000"):
-                part1 += hashed.hexdigest()[5]
+            hashed = hashlib.md5(f"{inp}{str(i)}".encode()).hexdigest()
+            if hashed.startswith("00000"):
+                part1 += hashed[5]
                 break
             i += 1
         print(i, part1)
         i += 1
     print(f"Part 1: {part1}")
+
+    # part 2
+    part2 = [' ' for _ in range(8)]
+    i = 0
+    for _ in range(8):
+        while True:
+            hashed = hashlib.md5(f"{inp}{str(i)}".encode()).hexdigest()
+            if hashed.startswith("00000"):
+                pos = hashed[5]
+                if pos.isdecimal() and int(pos) < 8 and part2[int(pos)] == ' ':
+                    part2[int(pos)] = hashed[6]
+                    break
+            i += 1
+        print(i, ''.join(part2))
+        i += 1
+    print(f"Part 2: {''.join(part2)}")
 
 
 if __name__ == '__main__':
