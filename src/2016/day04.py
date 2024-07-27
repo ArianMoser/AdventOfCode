ac4079d306d4988dabe687470edbb422b144bf00
ArianMoser <moser.arian@gmail.com>
2024-07-27 18:11:31 +0200

    Solves part 2 of day04 (AoC 2016)

--- src/2016/day04.py
+++ src/2016/day04.py
@@ -4,7 +4,7 @@ import re
 from collections import Counter
 
 INPUT_DIR = os.path.join(os.path.dirname(__file__), 'inputs')
-INPUT_RE = r'([a-z]+?)(\d+)\[([a-z]+)\]'
+INPUT_RE = r'([a-z\-]+?)(\d+)\[([a-z]+)\]'
 
 
 def prepare_input_file(filename=os.path.basename(__file__).replace('.py',
@@ -14,17 +14,31 @@ def prepare_input_file(filename=os.path.basename(__file__).replace('.py',
         lines = f.read().splitlines()
     rooms = []
     for line in lines:
-        result = re.search(INPUT_RE, line.replace('-', ''))
+        result = re.search(INPUT_RE, line)
         rooms.append(result.groups())
     return rooms
 
 
 def is_real_room(room):
-    occurencies = Counter(sorted(room[0])).most_common()
+    encrypted_name = room[0].replace('-', '')
+    occurencies = Counter(sorted(encrypted_name)).most_common()
     checksum = ''
     for el in occurencies[:5]:
         checksum += el[0]
     return checksum == room[2]
+
+
+def decrypt_room_name(room):
+    rotation = int(room[1]) % 26
+
+    offset = 97
+    decrypted_name = ''
+    for c in room[0]:
+        if c == '-':
+            decrypted_name += ' '
+            continue
+        decrypted_name += chr(((ord(c) - offset + rotation) % 26) + offset)
+    return decrypted_name
 
 
 def main():
@@ -43,6 +57,11 @@ def main():
     print(f"Part 1: {part1}")
 
     # part 2
+    for room in rooms:
+        if is_real_room(room):
+            if 'north' in decrypt_room_name(room):
+                print(f"Part 2: {room[1]} ({decrypt_room_name(room)})")
+
 
 
 if __name__ == '__main__':
